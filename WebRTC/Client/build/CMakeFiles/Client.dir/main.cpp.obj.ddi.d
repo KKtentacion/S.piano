808152daa56cce5129b7c04fa2cae564d960d683
KKtentacion <535976386@qq.com>
2025-02-27 18:07:14 +0800

    feat/Client

---
+++ WebRTC/Client/build/CMakeFiles/Client.dir/main.cpp.obj.ddi.d
@@ -0,0 +1,304 @@
+CMakeFiles\Client.dir\main.cpp.obj.ddi: E:\S.piano\WebRTC\Client\main.cpp \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\iostream \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\yvals_core.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\vcruntime.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\sal.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\concurrencysal.h \
+  E:\LLVM\lib\clang\18\include\vadefs.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\vadefs.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\xkeycheck.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\istream \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\ostream \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\ios \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\xlocnum \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\climits \
+  E:\LLVM\lib\clang\18\include\limits.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\limits.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\cmath \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\yvals.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.22621.0\ucrt\crtdbg.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.22621.0\ucrt\corecrt.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\vcruntime_new_debug.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\vcruntime_new.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\crtdefs.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\use_ansi.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\cstdlib \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.22621.0\ucrt\math.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.22621.0\ucrt\corecrt_math.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.22621.0\ucrt\stdlib.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.22621.0\ucrt\corecrt_malloc.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.22621.0\ucrt\corecrt_search.h \
+  E:\LLVM\lib\clang\18\include\stddef.h \
+  E:\LLVM\lib\clang\18\include\__stddef_ptrdiff_t.h \
+  E:\LLVM\lib\clang\18\include\__stddef_size_t.h \
+  E:\LLVM\lib\clang\18\include\__stddef_wchar_t.h \
+  E:\LLVM\lib\clang\18\include\__stddef_null.h \
+  E:\LLVM\lib\clang\18\include\__stddef_nullptr_t.h \
+  E:\LLVM\lib\clang\18\include\__stddef_max_align_t.h \
+  E:\LLVM\lib\clang\18\include\__stddef_offsetof.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.22621.0\ucrt\corecrt_wstdlib.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\xtr1common \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\xutility \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\__msvc_iter_core.hpp \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\utility \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\initializer_list \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\cstddef \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\type_traits \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\cstdint \
+  E:\LLVM\lib\clang\18\include\stdint.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\stdint.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\concepts \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\compare \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\cstring \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.22621.0\ucrt\string.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.22621.0\ucrt\corecrt_memory.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.22621.0\ucrt\corecrt_memcpy_s.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.22621.0\ucrt\errno.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\vcruntime_string.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.22621.0\ucrt\corecrt_wstring.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\cstdio \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.22621.0\ucrt\stdio.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.22621.0\ucrt\corecrt_wstdio.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.22621.0\ucrt\corecrt_stdio_config.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\iterator \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\iosfwd \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\cwchar \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.22621.0\ucrt\wchar.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.22621.0\ucrt\corecrt_wconio.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.22621.0\ucrt\corecrt_wctype.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.22621.0\ucrt\corecrt_wdirect.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.22621.0\ucrt\corecrt_wio.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.22621.0\ucrt\corecrt_share.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.22621.0\ucrt\corecrt_wprocess.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.22621.0\ucrt\corecrt_wtime.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.22621.0\ucrt\sys\stat.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.22621.0\ucrt\sys\types.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\streambuf \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\xiosbase \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.22621.0\ucrt\share.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\system_error \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\__msvc_system_error_abi.hpp \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\cerrno \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\stdexcept \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\exception \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.22621.0\ucrt\malloc.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\vcruntime_exception.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\eh.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.22621.0\ucrt\corecrt_terminate.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\xstring \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\__msvc_sanitizer_annotate_container.hpp \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\xmemory \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\limits \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\cfloat \
+  E:\LLVM\lib\clang\18\include\float.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.22621.0\ucrt\float.h \
+  E:\LLVM\lib\clang\18\include\intrin.h \
+  E:\LLVM\lib\clang\18\include\x86intrin.h \
+  E:\LLVM\lib\clang\18\include\ia32intrin.h \
+  E:\LLVM\lib\clang\18\include\immintrin.h \
+  E:\LLVM\lib\clang\18\include\x86gprintrin.h \
+  E:\LLVM\lib\clang\18\include\hresetintrin.h \
+  E:\LLVM\lib\clang\18\include\uintrintrin.h \
+  E:\LLVM\lib\clang\18\include\usermsrintrin.h \
+  E:\LLVM\lib\clang\18\include\crc32intrin.h \
+  E:\LLVM\lib\clang\18\include\prfchiintrin.h \
+  E:\LLVM\lib\clang\18\include\raointintrin.h \
+  E:\LLVM\lib\clang\18\include\cmpccxaddintrin.h \
+  E:\LLVM\lib\clang\18\include\mmintrin.h \
+  E:\LLVM\lib\clang\18\include\xmmintrin.h \
+  E:\LLVM\lib\clang\18\include\mm_malloc.h \
+  E:\LLVM\lib\clang\18\include\emmintrin.h \
+  E:\LLVM\lib\clang\18\include\pmmintrin.h \
+  E:\LLVM\lib\clang\18\include\tmmintrin.h \
+  E:\LLVM\lib\clang\18\include\smmintrin.h \
+  E:\LLVM\lib\clang\18\include\popcntintrin.h \
+  E:\LLVM\lib\clang\18\include\wmmintrin.h \
+  E:\LLVM\lib\clang\18\include\__wmmintrin_aes.h \
+  E:\LLVM\lib\clang\18\include\__wmmintrin_pclmul.h \
+  E:\LLVM\lib\clang\18\include\clflushoptintrin.h \
+  E:\LLVM\lib\clang\18\include\clwbintrin.h \
+  E:\LLVM\lib\clang\18\include\avxintrin.h \
+  E:\LLVM\lib\clang\18\include\avx2intrin.h \
+  E:\LLVM\lib\clang\18\include\f16cintrin.h \
+  E:\LLVM\lib\clang\18\include\bmiintrin.h \
+  E:\LLVM\lib\clang\18\include\bmi2intrin.h \
+  E:\LLVM\lib\clang\18\include\lzcntintrin.h \
+  E:\LLVM\lib\clang\18\include\fmaintrin.h \
+  E:\LLVM\lib\clang\18\include\avx512fintrin.h \
+  E:\LLVM\lib\clang\18\include\avx512vlintrin.h \
+  E:\LLVM\lib\clang\18\include\avx512bwintrin.h \
+  E:\LLVM\lib\clang\18\include\avx512bitalgintrin.h \
+  E:\LLVM\lib\clang\18\include\avx512cdintrin.h \
+  E:\LLVM\lib\clang\18\include\avx512vpopcntdqintrin.h \
+  E:\LLVM\lib\clang\18\include\avx512vpopcntdqvlintrin.h \
+  E:\LLVM\lib\clang\18\include\avx512vnniintrin.h \
+  E:\LLVM\lib\clang\18\include\avx512vlvnniintrin.h \
+  E:\LLVM\lib\clang\18\include\avxvnniintrin.h \
+  E:\LLVM\lib\clang\18\include\avx512dqintrin.h \
+  E:\LLVM\lib\clang\18\include\avx512vlbitalgintrin.h \
+  E:\LLVM\lib\clang\18\include\avx512vlbwintrin.h \
+  E:\LLVM\lib\clang\18\include\avx512vlcdintrin.h \
+  E:\LLVM\lib\clang\18\include\avx512vldqintrin.h \
+  E:\LLVM\lib\clang\18\include\avx512erintrin.h \
+  E:\LLVM\lib\clang\18\include\avx512ifmaintrin.h \
+  E:\LLVM\lib\clang\18\include\avx512ifmavlintrin.h \
+  E:\LLVM\lib\clang\18\include\avxifmaintrin.h \
+  E:\LLVM\lib\clang\18\include\avx512vbmiintrin.h \
+  E:\LLVM\lib\clang\18\include\avx512vbmivlintrin.h \
+  E:\LLVM\lib\clang\18\include\avx512vbmi2intrin.h \
+  E:\LLVM\lib\clang\18\include\avx512vlvbmi2intrin.h \
+  E:\LLVM\lib\clang\18\include\avx512pfintrin.h \
+  E:\LLVM\lib\clang\18\include\avx512fp16intrin.h \
+  E:\LLVM\lib\clang\18\include\avx512vlfp16intrin.h \
+  E:\LLVM\lib\clang\18\include\avx512bf16intrin.h \
+  E:\LLVM\lib\clang\18\include\avx512vlbf16intrin.h \
+  E:\LLVM\lib\clang\18\include\pkuintrin.h \
+  E:\LLVM\lib\clang\18\include\vpclmulqdqintrin.h \
+  E:\LLVM\lib\clang\18\include\vaesintrin.h \
+  E:\LLVM\lib\clang\18\include\gfniintrin.h \
+  E:\LLVM\lib\clang\18\include\avxvnniint8intrin.h \
+  E:\LLVM\lib\clang\18\include\avxneconvertintrin.h \
+  E:\LLVM\lib\clang\18\include\sha512intrin.h \
+  E:\LLVM\lib\clang\18\include\sm3intrin.h \
+  E:\LLVM\lib\clang\18\include\sm4intrin.h \
+  E:\LLVM\lib\clang\18\include\avxvnniint16intrin.h \
+  E:\LLVM\lib\clang\18\include\rtmintrin.h \
+  E:\LLVM\lib\clang\18\include\xtestintrin.h \
+  E:\LLVM\lib\clang\18\include\shaintrin.h \
+  E:\LLVM\lib\clang\18\include\fxsrintrin.h \
+  E:\LLVM\lib\clang\18\include\xsaveintrin.h \
+  E:\LLVM\lib\clang\18\include\xsaveoptintrin.h \
+  E:\LLVM\lib\clang\18\include\xsavecintrin.h \
+  E:\LLVM\lib\clang\18\include\xsavesintrin.h \
+  E:\LLVM\lib\clang\18\include\cetintrin.h \
+  E:\LLVM\lib\clang\18\include\adcintrin.h \
+  E:\LLVM\lib\clang\18\include\adxintrin.h \
+  E:\LLVM\lib\clang\18\include\rdseedintrin.h \
+  E:\LLVM\lib\clang\18\include\wbnoinvdintrin.h \
+  E:\LLVM\lib\clang\18\include\cldemoteintrin.h \
+  E:\LLVM\lib\clang\18\include\waitpkgintrin.h \
+  E:\LLVM\lib\clang\18\include\movdirintrin.h \
+  E:\LLVM\lib\clang\18\include\pconfigintrin.h \
+  E:\LLVM\lib\clang\18\include\sgxintrin.h \
+  E:\LLVM\lib\clang\18\include\ptwriteintrin.h \
+  E:\LLVM\lib\clang\18\include\invpcidintrin.h \
+  E:\LLVM\lib\clang\18\include\amxfp16intrin.h \
+  E:\LLVM\lib\clang\18\include\keylockerintrin.h \
+  E:\LLVM\lib\clang\18\include\amxintrin.h \
+  E:\LLVM\lib\clang\18\include\amxcomplexintrin.h \
+  E:\LLVM\lib\clang\18\include\avx512vp2intersectintrin.h \
+  E:\LLVM\lib\clang\18\include\avx512vlvp2intersectintrin.h \
+  E:\LLVM\lib\clang\18\include\enqcmdintrin.h \
+  E:\LLVM\lib\clang\18\include\serializeintrin.h \
+  E:\LLVM\lib\clang\18\include\tsxldtrkintrin.h \
+  E:\LLVM\lib\clang\18\include\mm3dnow.h \
+  E:\LLVM\lib\clang\18\include\prfchwintrin.h \
+  E:\LLVM\lib\clang\18\include\ammintrin.h \
+  E:\LLVM\lib\clang\18\include\fma4intrin.h \
+  E:\LLVM\lib\clang\18\include\xopintrin.h \
+  E:\LLVM\lib\clang\18\include\tbmintrin.h \
+  E:\LLVM\lib\clang\18\include\lwpintrin.h \
+  E:\LLVM\lib\clang\18\include\mwaitxintrin.h \
+  E:\LLVM\lib\clang\18\include\clzerointrin.h \
+  E:\LLVM\lib\clang\18\include\rdpruintrin.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\setjmp.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\new \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\xatomic.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\tuple \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\xpolymorphic_allocator.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\xcall_once.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\xerrc.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\atomic \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\xatomic_wait.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\xthreads.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\xtimec.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\ctime \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.22621.0\ucrt\time.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\xlocale \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\memory \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\typeinfo \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\vcruntime_typeinfo.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\xfacet \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\xlocinfo \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\__msvc_xlocinfo_types.hpp \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\cctype \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.22621.0\ucrt\ctype.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\clocale \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.22621.0\ucrt\locale.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\string \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\thread \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\__msvc_chrono.hpp \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\ratio \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.22621.0\ucrt\process.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.22621.0\ucrt\corecrt_startup.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\vcruntime_startup.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\stop_token \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\chrono \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\xfilesystem_abi.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\__msvc_tzdb.hpp \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\forward_list \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\optional \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\xsmf_control.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\sstream \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\vector \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\__msvc_bit_utils.hpp \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\xloctime \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\format \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\__msvc_format_ucd_tables.hpp \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\__msvc_print.hpp \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\bit \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\charconv \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\xbit_ops.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\xcharconv.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\xcharconv_ryu.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\xcharconv_ryu_tables.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\xcharconv_tables.h \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\locale \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\xlocbuf \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\xlocmes \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\xlocmon \
+  E:\VS\VC\Tools\MSVC\14.38.33130\include\iomanip \
+  E:\ffmpeg-master-latest-win64-gpl-shared\include\libavformat\avformat.h \
+  E:\ffmpeg-master-latest-win64-gpl-shared\include\libavcodec\codec_par.h \
+  E:\ffmpeg-master-latest-win64-gpl-shared\include\libavutil\avutil.h \
+  E:\ffmpeg-master-latest-win64-gpl-shared\include\libavutil\common.h \
+  E:\LLVM\lib\clang\18\include\inttypes.h \
+  C:\Program\ Files\ (x86)\Windows\ Kits\10\Include\10.0.22621.0\ucrt\inttypes.h \
+  E:\ffmpeg-master-latest-win64-gpl-shared\include\libavutil\attributes.h \
+  E:\ffmpeg-master-latest-win64-gpl-shared\include\libavutil\error.h \
+  E:\ffmpeg-master-latest-win64-gpl-shared\include\libavutil\macros.h \
+  E:\ffmpeg-master-latest-win64-gpl-shared\include\libavutil\avconfig.h \
+  E:\ffmpeg-master-latest-win64-gpl-shared\include\libavutil\version.h \
+  E:\ffmpeg-master-latest-win64-gpl-shared\include\libavutil\mem.h \
+  E:\ffmpeg-master-latest-win64-gpl-shared\include\libavutil\rational.h \
+  E:\ffmpeg-master-latest-win64-gpl-shared\include\libavutil\mathematics.h \
+  E:\ffmpeg-master-latest-win64-gpl-shared\include\libavutil\intfloat.h \
+  E:\ffmpeg-master-latest-win64-gpl-shared\include\libavutil\log.h \
+  E:\LLVM\lib\clang\18\include\stdarg.h \
+  E:\LLVM\lib\clang\18\include\__stdarg___gnuc_va_list.h \
+  E:\LLVM\lib\clang\18\include\__stdarg_va_list.h \
+  E:\LLVM\lib\clang\18\include\__stdarg_va_arg.h \
+  E:\LLVM\lib\clang\18\include\__stdarg___va_copy.h \
+  E:\LLVM\lib\clang\18\include\__stdarg_va_copy.h \
+  E:\ffmpeg-master-latest-win64-gpl-shared\include\libavutil\pixfmt.h \
+  E:\ffmpeg-master-latest-win64-gpl-shared\include\libavutil\channel_layout.h \
+  E:\ffmpeg-master-latest-win64-gpl-shared\include\libavcodec\codec_id.h \
+  E:\ffmpeg-master-latest-win64-gpl-shared\include\libavutil\samplefmt.h \
+  E:\ffmpeg-master-latest-win64-gpl-shared\include\libavcodec\version_major.h \
+  E:\ffmpeg-master-latest-win64-gpl-shared\include\libavcodec\defs.h \
+  E:\ffmpeg-master-latest-win64-gpl-shared\include\libavcodec\packet.h \
+  E:\ffmpeg-master-latest-win64-gpl-shared\include\libavutil\buffer.h \
+  E:\ffmpeg-master-latest-win64-gpl-shared\include\libavutil\dict.h \
+  E:\ffmpeg-master-latest-win64-gpl-shared\include\libavformat\avio.h \
+  E:\ffmpeg-master-latest-win64-gpl-shared\include\libavformat\version_major.h \
+  E:\ffmpeg-master-latest-win64-gpl-shared\include\libavformat\version.h \
+  E:\ffmpeg-master-latest-win64-gpl-shared\include\libavutil\frame.h \
+  E:\ffmpeg-master-latest-win64-gpl-shared\include\libavcodec\codec.h \
+  E:\ffmpeg-master-latest-win64-gpl-shared\include\libavutil\hwcontext.h \
+  E:\ffmpeg-master-latest-win64-gpl-shared\include\libavcodec\avcodec.h \
+  E:\ffmpeg-master-latest-win64-gpl-shared\include\libavcodec\version.h \
+  E:\ffmpeg-master-latest-win64-gpl-shared\include\libavcodec\codec_desc.h \
+  E:\ffmpeg-master-latest-win64-gpl-shared\include\libswscale\swscale.h \
+  E:\ffmpeg-master-latest-win64-gpl-shared\include\libswscale\version_major.h \
+  E:\ffmpeg-master-latest-win64-gpl-shared\include\libswscale\version.h \
+  E:\ffmpeg-master-latest-win64-gpl-shared\include\libavutil\opt.h
